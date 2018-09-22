21152e98e5fe1ed52382d5bdbdd21e380ec445e0
joeyzhuangyi <41659125+joeyzhuangyi@users.noreply.github.com>
2018-09-22 13:12:07 +1000

    class

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from classes import user
 
 app = Flask(__name__)
+student1 = user("olsen matthew","female",3,["Repeating comp1511"],"lazykid")
+student2 = user("olsen matthew","female",3,["Repeating comp1511"],"lazykid")
 
+sample_user_tutor = user("jfk","male",4,["Date Science","AI","Java development","COMP 1521"],["You sucks","Nice experience"])
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
 if __name__ == '__main__':
     app.run()
+
+
--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from flask_login import UserMixin
 from datetime import datetime, timedelta
 class user:
-    def __init__(self,name,gender,rating,related_tag,about,reviews=[],past_tutoring = [],point=10):
+    def __init__(self,name,gender,rating,related_tag,about,reviews=[],past_tutoring = [],point=10,bank = 100):
         user._name = name
         user._gender= gender
         user._rating = rating
@@ -12,6 +12,7 @@ class user:
         user._past_tutoring = past_tutoring
         user._availability = False
         user._point = point
+        user._bank = bank
 
     def add_reviews(self,user_other,review):
         if user_other in self.past_tutoring:
@@ -22,3 +23,7 @@ class user:
         else:
             self._availability = True
 
+    def add_past_tutoring(self,past):
+        self._past_tutoring.append(past)
+    def add_tag(self,new_tag):
+        self._related_tag.append(new_tag)
