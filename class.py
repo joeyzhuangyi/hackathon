dd6f3979b58c76c82da5161b10da0c99b806097f
joeyzhuangyi <41659125+joeyzhuangyi@users.noreply.github.com>
2018-09-22 11:23:52 +1000

    class

--- class.py
+++ class.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from flask_login import UserMixin
 from datetime import datetime, timedelta
 class user:
-    def __init__(self,name,gender,rating,related_tag,about,reviews,past_tutoring = []):
+    def __init__(self,name,gender,rating,related_tag,about,reviews=[],past_tutoring = [],point=10):
         user._name = name
         user._gender= gender
         user._rating = rating
@@ -10,6 +10,15 @@ class user:
         user._about = about
         user._reviews = reviews
         user._past_tutoring = past_tutoring
+        user._availability = False
+        user._point = point
 
-    def add_reviews(self,user_other):
-        pass
+    def add_reviews(self,user_other,review):
+        if user_other in self.past_tutoring:
+            self._review.append(review)
+    def change_availability (self):
+        if self._availability == True:
+            self._availability= False
+        else:
+            self._availability = True
+
